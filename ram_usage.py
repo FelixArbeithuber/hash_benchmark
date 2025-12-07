3e9cab05c10f084d552e8830e0e96dfa11cbf38f
Felix Arbeithuber <felix.arbeithuber@gmail.com>
2025-12-07 16:21:45 +0100

    improce ram_usage test

--- ram_usage.py
+++ ram_usage.py
@@ -1,18 +1,25 @@
 import gc
 import hashlib
+import os
 
 from eth_hash.auto import keccak
 from memory_profiler import profile
 
+input_data = os.urandom(8192 * 8192)
 
 @profile
 def measure_hash_footprints():
-    sha256_hash = hashlib.sha256(b"").digest()
-    blake2b_hash = hashlib.blake2b(b"").digest()
-    keccak_hash = keccak(b"")
+    sha256 = hashlib.sha256()
+    sha256.update(input_data)
+    _ = sha256.digest()
+    
+    blake2b = hashlib.blake2b()
+    blake2b.update(input_data)
+    _ = blake2b.digest()
+
+    _ = keccak(input_data)
 
 
 if __name__ == "__main__":
     gc.disable()
     measure_hash_footprints()
-    gc.enable()
